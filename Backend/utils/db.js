fcf55828405867309aa77b0226826a9cfb540bd6
taha7486 <tahaessamit@gmail.com>
2025-07-06 12:53:08 +0100

    Working

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -5,11 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined in .env file");
-    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
